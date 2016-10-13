374501466ebf68ee1e3cfe0ef105e2bf508a3f41
Louie Hernandez <lhernandez6@luc.edu>
2016-10-13 17:21:17 -0500

    Updating the event window

--- Display.py
+++ Display.py
@@ -3,3 +3,11 @@ import pygame
 pygame.init()
 gameDisplay =pygame.display.set_mode((800,600))
 pygame.display.set_caption("A bit Shootey")
+clock=pygame.time.Clock()
+
+crashed=False
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed =True
+        print(event)
