e618dbda3a4a53cbc9f26cdb3bfdb638e5ab5604
louiehernandez95 <louieghernandez95@gmail.com>
2016-11-23 19:54:58 -0600

    can now save highscores with text file

--- highscores.py
+++ highscores.py
@@ -5,28 +5,35 @@ import state
 import title
 
 
+
 class HighScores(state.State):
     high_scores = []
-
+    with open('highscores.txt') as f:
+        for line in f:
+            high_scores.append(int(line))
     def __init__(self, score):
         self.display = pygame.display.get_surface()
         self.background = pygame.image.load("data/images/space.png")
 
         if score > 0:
             HighScores.high_scores.append(score)
-
         HighScores.high_scores.sort(reverse=True)
 
         if len(HighScores.high_scores) > 10:
             del HighScores.high_scores[10:]
+        filename='highscores.txt'
+        with open(filename, 'w') as f:
+            for line in HighScores.high_scores:
+                f.write(str(line))
+                f.write('\n')
+
 
         self.header_manager = pygame.font.SysFont("comicssansms", 84)
         self.header = self.header_manager.render("HIGHSCORES:", True, (255, 255, 255))
         self.header_rect = pygame.Rect((self.display.get_width() / 2 - self.header.get_width() / 2, 0),
                                        (self.header.get_width(), self.header.get_height()))
-
-        self.Try_manager = pygame.font.SysFont("comicssansms", 64)
         self.font_manager = pygame.font.SysFont("comicssansms", 28)
+        self.Try_manager = pygame.font.SysFont("comicssansms", 64)
         self.Try_Again=self.Try_manager.render("PRESS ENTER TO TRY AGAIN", True, (255,255,255))
         self.Try_Again_rect = pygame.Rect((self.display.get_width() / 2 - self.Try_Again.get_width() / 2,500),
                                         (self.Try_Again.get_width(), self.Try_Again.get_height()))
@@ -48,7 +55,7 @@ class HighScores(state.State):
         self.display.blit(self.Try_Again, self.Try_Again_rect)
 
         for y, score in enumerate(HighScores.high_scores):
-            self.display.blit(self.font_manager.render("%d    %d" % (y + 1, score), True, (255, 255, 255)), (
+            self.display.blit(self.font_manager.render('{0:d}    {1:d}'.format(y + 1, score), True, (255, 255, 255)), (
             self.display.get_width() / 2 - 64, (self.header_rect.top + self.header.get_height()) + (32 * (y + 1))))
 
         pygame.display.update()
