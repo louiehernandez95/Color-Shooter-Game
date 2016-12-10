2f9c88e0b6cecc0c9050c1655e0ff4be9af1154e
louiehernandez95 <louieghernandez95@gmail.com>
2016-12-09 23:02:23 -0600

    replay now added

--- highscores.py
+++ highscores.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 
 import state
 import title
+import game
 
 
 
@@ -33,11 +34,13 @@ class HighScores(state.State):
         self.header_rect2 = pygame.Rect((self.display.get_width() / 2 - self.header.get_width() / 2, 50),
                                        (self.header.get_width(), self.header.get_height()))
         self.font_manager = pygame.font.SysFont("comicssansms", 28)
-        self.Try_manager = pygame.font.SysFont("comicssansms", 54)
-        self.Try_Again=self.Try_manager.render("PRESS ENTER TO TRY AGAIN", True, (255,255,255))
-        self.Try_Again_rect = pygame.Rect((self.display.get_width() / 2 - self.Try_Again.get_width() / 2,500),
+        self.Try_manager = pygame.font.SysFont("comicssansms", 38)
+        self.Try_Again=self.Try_manager.render("PRESS THE SPACEBAR TO TRY AGAIN", True, (255,255,255))
+        self.Try_Again2 = self.Try_manager.render("PRESS 'ENTER' TO RETURN TO THE MAIN MENU", True, (255, 255, 255))
+        self.Try_Again_rect = pygame.Rect((self.display.get_width() / 2 - self.Try_Again.get_width() / 2,450),
                                         (self.Try_Again.get_width(), self.Try_Again.get_height()))
-
+        self.Try_Again_rect2 = pygame.Rect((self.display.get_width() / 2 - self.Try_Again2.get_width() / 2, 500),
+                                          (self.Try_Again2.get_width(), self.Try_Again2.get_height()))
         self.music = pygame.mixer.Sound("data/sound/title_highscore.wav")
         self.music.play(loops=-1)
 
@@ -48,10 +51,13 @@ class HighScores(state.State):
         keys = pygame.key.get_pressed()
         if keys[K_RETURN]:
             return title.Title()
+        if keys[K_SPACE]:
+	       return game.Game()
     def act(self):
         self.display.blit(self.header2, self.header_rect2)
         self.display.blit(self.header, self.header_rect)
         self.display.blit(self.Try_Again, self.Try_Again_rect)
+        self.display.blit(self.Try_Again2, self.Try_Again_rect2)
 
         for y, score in enumerate(HighScores.high_scores):
             self.display.blit(self.font_manager.render('{0:d}    {1:d}'.format(y + 1, score), True, (255, 255, 255)), (
